3bac621fc8355f736a480fc6d43c76ee1b93b948
Riley <142259664+itsjustriley@users.noreply.github.com>
2023-08-24 13:55:25 -0300

    Update custom-calc-stat.py

--- custom-calc-stat.py
+++ custom-calc-stat.py
@@ -16,15 +16,16 @@ def stat_calc(first,second,third,fourth):
 
 # Keep it DRY. Probably a way to have it do 1st, 2nd, 3rd automatically. I like the text though.
 pt1 = "What is the result on the"
-pt2 = "d6? "
+# originally had a variable for "d6? " but realized it's just as much work as typing the prhase to use the variable
+# lesson learned - don't make more work for yourself avoiding repeption. 
 
 print("Calculator: D&D stat.")
 print("Roll 4d6.")
       
-first = int(input(f"{pt1} first {pt2}"))
-second = int(input(f"{pt1} second {pt2}"))
-third = int(input(f"{pt1} third {pt2}"))
-fourth = int(input(f"{pt1} fourth {pt2}"))
+first = int(input(f"{pt1} first d6? "))
+second = int(input(f"{pt1} second d6? "))
+third = int(input(f"{pt1} third d6? "))
+fourth = int(input(f"{pt1} fourth d6? "))
 
 result = stat_calc(first,second,third,fourth)
 print(result)
